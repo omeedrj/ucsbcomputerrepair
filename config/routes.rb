d89c75a4244e971515a1a6e6ebdacae8bdfd8198
Omeed Rabani <omeedrabani@omeed-rabanis-macbook-pro.local>
2015-08-23 08:30:32 -0700

    Fixed typo within routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root 'static_page#home'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
