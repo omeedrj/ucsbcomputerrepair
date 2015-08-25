9f4da56cdd050cf21f3de257bfeaa9313f1fc65b
Omeed Rabani <omeedrabani@omeed-rabanis-macbook-pro.local>
2015-08-25 00:46:20 -0700

    Make phone number required. Improve format of new repair form. Add more space to the bottom of the show view to more easily see everything at the bottom.

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -5,16 +5,17 @@ class Repair < ActiveRecord::Base
 
 	validates :customer_first_name, presence: true, length: { maximum: 35 }
 	validates :customer_last_name, 	presence: true, length: { maximum: 35 }
-	validates :customer_id_number, length: { maximum: 20 }, numericality: true
-    validates :device_description, presence: true
+	validates :customer_id_number, length: { maximum: 20 }, numericality: true, allow_blank: true
+	validates :customer_phone_number, presence: true
+  validates :device_description, presence: true
 	validates :customer_email, presence: true, length: { maximum: 255 }, 
-							   format: { with: VALID_EMAIL_REGEX }
-    validates :customer_phone_number, presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }, 
-    								  allow_blank: true
+						format: { with: VALID_EMAIL_REGEX }
+  validates :customer_phone_number, presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }, 
+						allow_blank: true
 	validates :device_problem_description, presence: true
-    validates :terms_of_service_accepted, acceptance: true
-    validates :services_fee, presence: true
-    validates :parts_fee, presence: true
+  validates :terms_of_service_accepted, acceptance: true
+  validates :services_fee, presence: true
+  validates :parts_fee, presence: true
 
 	enum status: [ :active, :ready_for_pickup, :inactive ]
 
