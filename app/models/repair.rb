5d3e2ae4fd07dd1f241507b24131972f2c12ec09
Omeed Rabani <omeedrabani@omeed-rabanis-macbook-pro.local>
2015-08-29 15:15:18 -0700

    Create dependency between customer phone number and customer email for repair forms.
    This is essentially: phone number OR email

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -6,12 +6,9 @@ class Repair < ActiveRecord::Base
 	validates :customer_first_name, presence: true, length: { maximum: 35 }
 	validates :customer_last_name, 	presence: true, length: { maximum: 35 }
 	validates :customer_id_number, length: { maximum: 20 }, numericality: true, allow_blank: true
-	validates :customer_phone_number, presence: true
   validates :device_description, presence: true
-	validates :customer_email, presence: true, length: { maximum: 255 }, 
-						format: { with: VALID_EMAIL_REGEX }
-  validates :customer_phone_number, presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }, 
-						allow_blank: true
+	validates :customer_email, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, presence: true, :unless => :customer_phone_number? 
+  validates :customer_phone_number, format: { with: VALID_PHONE_NUMBER_REGEX }, presence: true, :unless => :customer_email?
 	validates :device_password, confirmation: true
 	validates :device_problem_description, presence: true
   validates :terms_of_service_accepted, acceptance: true
