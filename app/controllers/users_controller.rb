095f5beec35d52c1f5315de72035e9f33845c57d
Omeed Rabani <omeedrabani@omeed-rabanis-macbook-pro.local>
2015-08-30 12:07:36 -0700

    Formatted new repair form a bit.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_filter :login_required
+before_filter :login_required
 	before_filter :admin_approval, :only => [:new, :create, :admin_edit_employee]
 
 	def index
