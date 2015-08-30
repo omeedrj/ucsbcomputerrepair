652efec96e0343e3792b89f77d0d2ac159b9a35e
Omeed Rabani <omeedrabani@omeed-rabanis-macbook-pro.local>
2015-08-29 18:14:42 -0700

    Create user model
    Create user model and associated necessities (controllers, views, etc)
    For security, in order for the user to edit certain atttributes their account must be authenticated with their current password.
    Need to implement reset password by email.

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :users
+
 
 
   # Example resource route with options:
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823190623) do
+ActiveRecord::Schema.define(version: 20150829223332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,19 @@ ActiveRecord::Schema.define(version: 20150823190623) do
     t.integer  "status",                                             default: 0
     t.datetime "created_at",                                                       null: false
     t.datetime "updated_at",                                                       null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "reset_digest"
+    t.string   "username"
+    t.integer  "role",            default: 1
+    t.string   "website_code"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
 end
