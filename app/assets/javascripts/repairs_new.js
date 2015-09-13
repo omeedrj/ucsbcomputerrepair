b2accba30cb70970b9756708b798891f5316a74e
Omeed Rabani <omeedrabani@omeed-rabanis-macbook-pro.local>
2015-09-13 08:30:26 -0700

    Add semicolons for syntax to hopefully fix problem of jSignature not showing up in the repair request form on heroku

--- app/assets/javascripts/repairs_new.js
+++ app/assets/javascripts/repairs_new.js
@@ -2,16 +2,16 @@ $(document).ready(function(){
 	$("#signature_parent_div").css("height", "290px");
 	$("#signature_parent_div").css("width", "90%");
 
-	var $sigdiv = $("#signature")
-	$sigdiv.jSignature()
+	var $sigdiv = $("#signature");
+	$sigdiv.jSignature();
 
 	$("#reset_signature_button").click(function(){
-		$sigdiv.jSignature("reset")
+		$sigdiv.jSignature("reset");
 	})
 
 	$("#signature").bind('change', function(e){ 
 		/* 'e.target' will refer to div with "#signature" */ 
-		var signature_data_pair = $sigdiv.jSignature("getData", "svg")
-		$("#repair_dropoff_signature_data").val(signature_data_pair[1])
+		var signature_data_pair = $sigdiv.jSignature("getData", "svg");
+		$("#repair_dropoff_signature_data").val(signature_data_pair[1]);
 	})
 })
