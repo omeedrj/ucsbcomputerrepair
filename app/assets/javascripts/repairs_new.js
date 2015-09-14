1c30b7f55f40c6c085a24a8d26b0c9b6b92487ef
Omeed Rabani <omeedrabani@omeed-rabanis-macbook-pro.local>
2015-09-13 22:04:30 -0700

    Add javascript such that the submit repair request button will only show after the user has put something in the jSignature.

--- app/assets/javascripts/repairs_new.js
+++ app/assets/javascripts/repairs_new.js
@@ -2,13 +2,24 @@ $(document).ready(function(){
 	var $sigdiv = $("#signature");
 	$sigdiv.jSignature();
 
+	if ($("#submit_button_div").is(":visible")) {
+		$("#submit_button_div").hide();
+	}
+
+
 	$("#reset_signature_button").click(function(){
 		$sigdiv.jSignature("reset");
+		if ($("#submit_button_div").is(":visible")) {
+			$("#submit_button_div").hide();
+		}
 	})
 
 	$("#signature").bind('change', function(e){ 
 		/* 'e.target' will refer to div with "#signature" */ 
 		var signature_data_pair = $sigdiv.jSignature("getData", "svg");
 		$("#repair_dropoff_signature_data").val(signature_data_pair[1]);
+		if ($("#submit_button_div").is(":hidden")) {
+			$("#submit_button_div").show();
+		}
 	})
 })
