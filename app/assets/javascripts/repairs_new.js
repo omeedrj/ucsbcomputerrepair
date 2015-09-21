ec316dffe1698a194e1d86011d9664aa9959cf35
Omeed Rabani <omeedrabani@omeed-rabanis-macbook-pro.local>
2015-09-21 15:38:47 -0700

    Add javascript so that the repair request form becomes multi-parted.
    Also make error messages their own row so they don't coincide with other parts of other pages.

--- app/assets/javascripts/repairs_new.js
+++ app/assets/javascripts/repairs_new.js
@@ -1,10 +1,40 @@
 $(document).ready(function(){
 	var $sigdiv = $("#signature");
-	$sigdiv.jSignature();
 
-	if ($("#submit_button_div").is(":visible")) {
-		$("#submit_button_div").hide();
-	}
+	$("#step_2_button").click(function(){
+		$("#step_1_div").hide();
+		$("#step_3_div").hide();
+		$("#step_4_div").hide();
+		$("#step_2_div").show();
+	})
+
+	$("#step_3_button").click(function(){
+		$("#step_1_div").hide();
+		$("#step_2_div").hide();
+		$("#step_4_div").hide();
+		$("#step_3_div").show();
+	})
+
+	$("#step_4_button").click(function(){
+		$("#step_1_div").hide();
+		$("#step_2_div").hide();
+		$("#step_3_div").hide();
+		$("#step_4_div").show();
+		
+		$("#review_customer_first_name").text($("#repair_customer_first_name").val());
+    $("#review_customer_last_name").text($("#repair_customer_last_name").val());
+    $("#review_customer_id_number").text($("#repair_customer_id_number").val());
+    $("#review_customer_email").text($("#repair_customer_email").val());
+    $("#review_customer_phone_number").text($("#repair_customer_phone_number").val());
+    $("#review_device_description").text($("#repair_device_description").val());
+    $("#review_device_serial_number").text($("#repair_device_serial_number").val());
+    $("#review_accessories_description").text($("#repair_accessories_description").val());
+    $("#review_device_problem_description").text($("#repair_device_problem_description").val());
+
+		$sigdiv.jSignature();
+
+		// Also add the form values into the appropriate DOM fields
+	})
 
 
 	$("#reset_signature_button").click(function(){
